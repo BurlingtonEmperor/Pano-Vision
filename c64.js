9248fade0690ec3349d877d9cd1512eafea5d3b2
Alexander Chang <alexanderchang208@gmail.com>
2025-08-11 17:20:47 -0400

    create basic c64 style console

---
+++ c64.js
@@ -0,0 +1,11 @@
+const consoleOutput = document.getElementById("console-output");
+const userInput = document.getElementById("user-input");
+
+// userInput.addEventListener('keydown', (event) => {
+//   if (event.key === 'Enter') {
+//     event.preventDefault(); 
+
+//     let user_true_input = userInput.innerText;
+//     consoleOutput.innerHTML += "<p>" + user_true_input + "</p>";
+//   }
+// });
