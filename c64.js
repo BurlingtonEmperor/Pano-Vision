b0aac6da85f0c595d89aab85550402eb8fc57d20
Alexander Chang <alexanderchang208@gmail.com>
2025-08-13 21:09:53 -0400

    add loader

--- c64.js
+++ c64.js
@@ -1,5 +1,9 @@
 const consoleOutput = document.getElementById("console-output");
 const userInput = document.getElementById("user-input");
+const buttonOptions = document.getElementById("button-options");
+
+const commodoreConsole = document.getElementById("console");
+const commodoreCamera = document.getElementById("camera");
 
 // userInput.addEventListener('keydown', (event) => {
 //   if (event.key === 'Enter') {
@@ -17,4 +21,27 @@ function addConsoleOutput (outputContent) {
 // pre-programmed stuff
 setTimeout(function () {
   addConsoleOutput("Options Below");
+  buttonOptions.style.display = "block";
 }, 700);
+
+function tunnelShow () {
+  commodoreConsole.style.display = "none";
+  commodoreCamera.style.display = "block";
+}
+
+// buttons
+const wildwoodBurlington = document.getElementById("wildwood-burlington");
+const reganBurlington = document.getElementById("regan-burlington");
+const targetBurlington = document.getElementById("target-burlington");
+
+wildwoodBurlington.onclick = function () {
+  tunnelShow();
+}
+
+reganBurlington.onclick = function () {
+  tunnelShow();
+}
+
+targetBurlington.onclick = function () {
+  tunnelShow();
+}
